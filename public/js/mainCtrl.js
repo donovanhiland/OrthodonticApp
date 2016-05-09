4828092b70b65a550180e3970c3f842631047c80
Donovan Hiland <donovan.hiland@gmail.com>
2016-05-09 09:46:03 -0600

    re-styled frontend for mobilefirst

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -1,8 +1,38 @@
 angular.module('orthoApp')
   .controller('mainCtrl', function($scope, mainService) {
 
+    $scope.sociallinks = [
+    {
+      name: 'facebook',
+      id: 'fb',
+      path: './img/icons/facebook.png',
+      alt: 'facebook link'
+    },
+    {
+      name: 'instagram',
+      id: 'ig',
+      path: './img/icons/instagram.png',
+      alt: 'instagram link'
+    },
+    {
+      name: 'twitter',
+      id: 'tt',
+      path: './img/icons/twitter.png',
+      alt: 'twitter link'
+    },
+    {
+      name: 'googleplus',
+      id: 'gp',
+      path: './img/icons/googleplus.png',
+      alt: 'google plus link'
+    },
+    {
+      name: 'linkedin',
+      id: 'li',
+      path: './img/icons/linkedin.png',
+      alt: 'linked in link'
+    }];
+
     $scope.homesectionlinks = ['New Patients', 'Patient Testimonials', 'Patient Registration', 'Contact Us'];
-
-
 
   });
